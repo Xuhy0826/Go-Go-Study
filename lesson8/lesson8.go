19e4314eac95a88cd74d929542790b265855f61e
Xuhy <HY_Xu0826@163.com>
2020-10-07 09:47:27 +0800

    方法

--- lesson8/lesson8.go
+++ lesson8/lesson8.go
@@ -4,9 +4,22 @@ import (
 	"fmt"
 )
 
+//type关键字声明新类型
+type celsius float64
+type kelvin float64
+
+//kelvinToCelsius converts °K to °C
+func kelvinToCelsius(k kelvin) celsius {
+	return celsius(k - 273.15) //需要使用类型转换
+}
+
+//为kelvin类型声明方法
+func (k kelvin) celsius() celsius {
+	return celsius(k - 273.15)
+}
+
 func main() {
-	//type关键字声明新类型
-	type celsius float64
+	//使用声明的自定义类型
 	var temperature celsius = 20
 	fmt.Println(temperature) //20
 
@@ -21,13 +34,22 @@ func main() {
 	//temperature += warmUp //报错
 
 	//通过自定义类型可提高代码的可读性，比如20摄氏度和20华氏度就不是一回事儿
-	type fahrenheit float64
+	//type fahrenheit float64
 
-	var c celsius = 20
-	var f fahrenheit = 20
+	//var c celsius = 20
+	//var f fahrenheit = 20
 
 	// if c == f {  //报错
 
 	// }
 	// c += f  //报错
+
+	//使用引入了新类型的函数
+	var k kelvin = 294.0
+	c := kelvinToCelsius(k)
+	fmt.Println(k, "°K is", c, "°C") //294 °K is 20.850000000000023 °C
+
+	//使用方法
+	c1 := k.celsius()
+	fmt.Println(k, "°K is", c1, "°C") //294 °K is 20.850000000000023 °C
 }
